aec725eed60dfbb2f38e57156db17367da87b6c6
Éderson Fernandes <edersonluis.rf@gmail.com>
2022-09-21 00:39:37 -0300

    Navegando em objetos com strings

--- src/components/commons/Footer/index.js
+++ src/components/commons/Footer/index.js
@@ -2,7 +2,7 @@ import { getCMSContent } from '../../../infra/cms/CMSProvider';
 import { Box, Link, Text, theme } from '../../../theme/components';
 
 export function Footer() {
-  const description = getCMSContent().globalContent?.globalFooter?.description;
+  const description = getCMSContent('globalContent.globalFooter.description');
   return (
     <Box
       tag="footer"
--- src/infra/cms/CMSProvider.js
+++ src/infra/cms/CMSProvider.js
@@ -1,11 +1,20 @@
 import React from "react";
+import get from 'lodash/get';
 
 const CMSContext = React.createContext({
   cmsContent: {}
 });
 
-export const getCMSContent = () => {
-  return React.useContext(CMSContext).cmsContent;
+export const getCMSContent = (path = '') => {
+  const cmsContent = React.useContext(CMSContext).cmsContent;
+
+  if (path === '') return cmsContent;
+
+  const output = get(cmsContent, path);
+
+  if (!output) throw new Error(`Não foi possivel encontrar a chave "${path}". Reveja sua query e tente novamente.`);
+
+  return output;
 }
 
 export default function CMSProvider({ cmsContent, children }) {
