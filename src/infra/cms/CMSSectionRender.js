015250e863ce748516c2d78f5ae4fd85a04b0eb6
Éderson Fernandes <ederson.fernandes@bempromotora.onmicrosoft.com>
2022-09-22 13:22:57 -0300

    Feature Flags via CMS

--- src/infra/cms/CMSSectionRender.js
+++ src/infra/cms/CMSSectionRender.js
@@ -6,8 +6,10 @@ export function CMSSectionRender({ pageName }) {
 
   return sections.map((sectionProps) => {
     const Component = cmsSections[sectionProps.componentName];
+    const isVisible = sectionProps.visible === true || sectionProps.visible === undefined;
 
     if (!Component) return null;
+    if (!isVisible) return null;
 
     return (
       <Component key={sectionProps.id} {...sectionProps} />
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -23,6 +23,7 @@ export async function getStaticProps({ preview }) {
               }
               ... on CommonFooterRecord {
                 id
+                visible
               }
               ... on PagehomeHerosectionRecord {
                 id
